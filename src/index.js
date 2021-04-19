bab42ad2dd51c8a20cd7bb920ceb55a5c06421fe
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-19 08:15:40 +0300

    Add DevInit assets to provide the right look and feel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 let app = document.querySelector('#app')
 
-app.innerHTML = '<h2>Welcome to di-chart-boilerplate</h2>'
+app.innerHTML = '<h2 class="section__heading">Welcome to di-chart-boilerplate</h2>'
