ac97b77fc2f1bbe42616de3414c94c283b9f4465
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-20 12:45:23 +0300

    Add filter

---
+++ src/utils/filters.js
@@ -0,0 +1,27 @@
+export const addFilterWrapper = (chartNode) => {
+  const filterWrapper = document.createElement('div');
+  filterWrapper.classList.add('data-selector--wrapper');
+  chartNode.parentElement.insertBefore(filterWrapper, chartNode);
+
+  return filterWrapper;
+};
+
+export const createOption = (selectElement, option) => {
+  const optionElement = document.createElement('option');
+  optionElement.value = typeof option === 'string' ? option : option.value;
+  optionElement.text = typeof option === 'string' ? option : option.label;
+  selectElement.appendChild(optionElement);
+};
+
+export const addFilter = ({
+  wrapper, options, allItemsLabel, className,
+}) => {
+  const selectElement = document.createElement('select');
+  selectElement.classList.add(...['data-selector', 'js-plotly-chart-data-selector', className]);
+  createOption(selectElement, { label: allItemsLabel, value: '*' });
+  options.forEach((option) => createOption(selectElement, option));
+  selectElement.classList.add('data-selector--active');
+  wrapper.appendChild(selectElement);
+
+  return selectElement;
+};
