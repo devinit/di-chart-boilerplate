fd61d4f9779e12712d19d2b06a6f6f8f5d6dffcc
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-01-18 18:43:05 +0300

    Render Country Filter

--- src/index.js
+++ src/index.js
@@ -1,52 +1,21 @@
-import deepMerge from 'deepmerge';
-import defaultOptions from './charts/echarts';
 import './styles/styles.css';
 import fetchCSVData from './utils/data';
 import { addFilter, addFilterWrapper } from './widgets/filters';
-import PillWidget from './widgets/pills';
 // import d3 from 'd3'; // eslint-disable-line import/no-unresolved
 
 // Your Code Goes Here i.e. functions
 
-const renderChart = (chartNode, data) => {
-  // append the svg object to the body of the page
-
-  // get the data
-  const chart = window.echarts.init(chartNode);
-  const option = {
-    legend: { show: false },
-    yAxis: {
-      type: 'category',
-      data: data.map((d) => d.Country),
-    },
-    xAxis: {
-      type: 'value',
-    },
-    series: [{
-      name: 'Countries',
-      data: data.map((d) => Number(d.Value)),
-      type: 'bar',
-      showBackground: true,
-      backgroundStyle: {
-        color: 'rgba(180, 180, 180, 0.2)',
-      },
-    }],
-  };
-  chart.setOption(deepMerge(defaultOptions, option));
-
-  return chart;
-};
 /**
  * Run your code after the page has loaded
  */
 window.addEventListener('load', () => {
   window.DICharts.handler.addChart({
-    className: 'dicharts--boilerplate-chart',
-    echarts: {
+    className: 'dicharts--oda-root',
+    d3: {
       onAdd: (chartNodes) => {
         Array.prototype.forEach.call(chartNodes, (chartNode) => {
           const dichart = new window.DICharts.Chart(chartNode.parentElement);
-          // dichart.showLoading();
+          dichart.showLoading();
 
           /**
            * ECharts - prefix all browsers global with window
@@ -54,62 +23,25 @@ window.addEventListener('load', () => {
            *
            * const chart = window.echarts.init(chartNode);
            */
-          const csv = 'https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv';
+          const csv = 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/rh-and-fp-dropdowns.csv';
           fetchCSVData(csv).then((data) => {
             const filterWrapper = addFilterWrapper(chartNode);
             const countryFilter = addFilter({
               wrapper: filterWrapper,
-              options: data.map((d) => d.Country),
-              allItemsLabel: 'All Countries',
+              options: data.map((d) => d.Donors),
+              defaultOption: 'United States',
               className: 'country-filter',
-              label: 'Select Country',
+              label: 'Select Donor',
             });
-            const chart = renderChart(chartNode, data);
-
-            // initialise pill widget for the multi-select option
-            const pillWidget = new PillWidget({ wrapper: filterWrapper });
-            if (pillWidget.pills.length) {
-              chartNode.parentElement.insertBefore(pillWidget.widget, chartNode);
-            }
-
-            const updateChart = (updatedData) => {
-              chart.setOption({
-                yAxis: {
-                  data: updatedData.map((d) => d.Country),
-                },
-                series: [
-                  {
-                    // find series by name
-                    name: 'Countries',
-                    data: updatedData.map((d) => d.Value),
-                  },
-                ],
-              });
-            };
 
             // add event listeners
             countryFilter.addEventListener('change', (event) => {
               const { value } = event.currentTarget;
-              if (value !== '*') {
-                // if it's the first pill, append pill widget
-                if (!pillWidget.pillNames.length) {
-                  chartNode.parentElement.insertBefore(pillWidget.widget, chartNode);
-                }
-                pillWidget.add(value);
-              } else {
-                pillWidget.removeAll();
-              }
-              // filter data to return only the selected items
-              const filteredData = value !== '*' ? data.filter((d) => pillWidget.pillNames.includes(d.Country)) : data;
-              // update chart
-              updateChart(filteredData);
+              console.log(value);
+              // update state
             });
 
-            pillWidget.onRemove(() => {
-              updateChart(data.filter((d) => pillWidget.pillNames.includes(d.Country)));
-            });
-
-            dichart.hideLoading();
+            // dichart.hideLoading();
           });
         });
       },
