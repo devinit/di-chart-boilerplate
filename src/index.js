fbe9c812d578be4efba1100621004b8fe3afc566
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-01-19 10:29:02 +0300

    Update CSV data file URL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './styles/styles.css';
 import fetchCSVData from './utils/data';
-import { addFilter, addFilterWrapper } from './widgets/filters';
 // import d3 from 'd3'; // eslint-disable-line import/no-unresolved
 
 // Your Code Goes Here i.e. functions
@@ -23,28 +22,12 @@ window.addEventListener('load', () => {
            *
            * const chart = window.echarts.init(chartNode);
            */
-          const csv = 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/rh-and-fp-dropdowns.csv';
+          const csv = 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/RH-and-FP-CRS-Data-2019.csv';
           fetchCSVData(csv).then((data) => {
-            const filterWrapper = addFilterWrapper(chartNode);
-            const countryFilter = addFilter({
-              wrapper: filterWrapper,
-              options: data.map((d) => d.Donors),
-              defaultOption: 'United States',
-              className: 'country-filter',
-              label: 'Select Donor',
-            });
-
+            console.log(data);
             if (window.DIState) {
               window.DIState.setState({ country: 'United States' });
             }
-
-            // add event listeners
-            countryFilter.addEventListener('change', (event) => {
-              const { value } = event.currentTarget;
-              if (window.DIState) {
-                window.DIState.setState({ country: value });
-              }
-            });
 
             dichart.hideLoading();
           });
