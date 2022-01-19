8d51e1470cfa4b68eb8b82f2e59f84f9a44dfe31
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-01-19 15:11:12 +0300

    Complete TableOne component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Table = (props) => <div className="table-styled">{props.children}</div>;
+const Table = (props) => (
+  <div className="table-styled">
+    <table>{props.children}</table>
+  </div>
+);
 
-Table.propTypes = { children: PropTypes.children };
+Table.propTypes = { children: PropTypes.any };
 
 export default Table;
--- src/components/TableOne/TableOne.js
+++ src/components/TableOne/TableOne.js
@@ -3,11 +3,27 @@ import PropTypes from 'prop-types';
 import Table from '../Table';
 
 const TableOne = (props) => {
-  return <Table>{props.country} Table Goes Here</Table>;
+  const renderRows = (rows, header = false) =>
+    rows.map((row, index) => (
+      <tr key={index}>{row.map((cell, key) => (header ? <th key={key}>{cell}</th> : <td key={key}>{cell}</td>))}</tr>
+    ));
+
+  return (
+    <Table>
+      <thead>
+        {renderRows(
+          props.rows.filter((row, index) => index === 0),
+          true,
+        )}
+      </thead>
+      <tbody>{renderRows(props.rows.filter((row, index) => index > 0))}</tbody>
+    </Table>
+  );
 };
 
 TableOne.propTypes = {
   country: PropTypes.string,
+  rows: PropTypes.arrayOf(PropTypes.array),
 };
 
 export { TableOne };
