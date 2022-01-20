68c47b11ec0afa17a1f3b79d323097b2bab6acc6
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-01-20 13:10:01 +0300

    Add table two

---
+++ src/core/TableTwo.js
@@ -0,0 +1,63 @@
+import { addFilter, addFilterWrapper } from '../widgets/filters';
+
+const init = (className) => {
+  window.DICharts.handler.addChart({
+    className,
+    echarts: {
+      onAdd: (chartNodes) => {
+        Array.prototype.forEach.call(chartNodes, (chartNode) => {
+          const dichart = new window.DICharts.Chart(chartNode.parentElement);
+          // dichart.showLoading();
+
+          /**
+           * ECharts - prefix all browsers global with window
+           * i.e window.echarts - echarts won't work without it
+           *
+           * const chart = window.echarts.init(chartNode);
+           */
+
+          dichart.showLoading();
+          const filterWrapper = addFilterWrapper(chartNode);
+          let purposeField;
+          if (window.DIState) {
+            window.DIState.addListener(() => {
+              dichart.showLoading();
+              const state = window.DIState.getState;
+              const { country, dataTwo: data } = state;
+              if (country && data) {
+                if (!purposeField) {
+                  purposeField = addFilter({
+                    wrapper: filterWrapper,
+                    options: data.reduce((options, prev) => {
+                      const value = prev['Code type']; // TODO: use a constant or named variable
+                      if (!options.includes(value)) {
+                        return options.concat(value);
+                      }
+
+                      return options;
+                    }, []),
+                    defaultOption: 'Reproductive health care and family planning',
+                    className: 'purpose-code-filter',
+                    label: 'Select Purpose Code',
+                  });
+                  purposeField.addEventListener('click', (event) => {
+                    console.log(event.target.value);
+                  });
+                }
+
+                // FIXME: table goes here
+
+                dichart.hideLoading();
+              }
+            });
+          } else {
+            console.log('State is not defined');
+            dichart.hideLoading();
+          }
+        });
+      },
+    },
+  });
+};
+
+export default init;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './state';
 import initTableOne from './core/TableOne';
 import initDonorFilter from './core/DonorFilter';
 import initBarChartOne from './core/BarChartOne';
+import initTableTwo from './core/TableTwo';
 import { fetchCoreData } from './utils/data';
 
 // Your Code Goes Here i.e. functions
@@ -15,4 +16,5 @@ window.addEventListener('load', () => {
   initDonorFilter('dicharts--oda-root');
   initTableOne('dicharts--table-one-root');
   initBarChartOne('dicharts--chart-one-root');
+  initTableTwo('dicharts--table-two-root');
 });
