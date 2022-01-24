dc86442600db73133e1b499a90fe55162d583531
David Ebukali <debukali@gmail.com>
2022-01-24 19:49:32 +0300

    Update chart when donor drop down changes

--- src/core/BarChartOne.js
+++ src/core/BarChartOne.js
@@ -1,6 +1,10 @@
 import deepMerge from 'deepmerge';
 import defaultOptions from '../charts/echarts';
+import { COUNTRY_FIELD, PURPOSE_FIELD, PURPOSE_TO_FILTER_BY } from '../utils/constants';
+import { filterDataByCountry, filterDataByPurpose } from '../utils/data';
 import { toJS } from 'mobx';
+
+let chart;
 
 const groupAndSum = (list) => {
   const existingItems = {};
@@ -10,7 +14,6 @@ const groupAndSum = (list) => {
 
       return;
     }
-    
     existingItems[item.year] = {
       ...existingItems[item.year],
       value: (parseFloat( item.value ? item.value : 0) + parseFloat(existingItems[item.year].value ? existingItems[item.year].value : 0)).toFixed(3),
@@ -22,8 +25,7 @@ const groupAndSum = (list) => {
 
 const extractChartFamilyPlanningData = (data) => {
   const chartData = toJS(data);
-
-  const groupedFamilyPlanningData = chartData.filter((data) => data.purpose_name === 'Family planning').map((data) => {
+  const groupedFamilyPlanningData = chartData.filter((data) => data.purpose_name === 'Family planning' && data.year >= 2010).map((data) => {
     return {
       'year': data.year,
       'value': data.usd_disbursement_deflated
@@ -35,8 +37,7 @@ const extractChartFamilyPlanningData = (data) => {
 
 const extractChartReproductiveHealthData = (data) => {
   const chartData = toJS(data);
-
-  const groupedReproductiveData = chartData.filter((data) => data.purpose_name === 'Reproductive health care').map((data) => {
+  const groupedReproductiveData = chartData.filter((data) => data.purpose_name === 'Reproductive health care' && data.year >= 2010).map((data) => {
     return {
       'year': data.year,
       'value': data.usd_disbursement_deflated
@@ -49,11 +50,11 @@ const extractChartReproductiveHealthData = (data) => {
 const extractChartYears = (data) => {
   const chartData = toJS(data);
 
-  return chartData.map((data) => data.year).filter((value, index, self) => self.indexOf(value) === index).sort();
+  return chartData.filter((data) => data.year >= 2010).map((data) => data.year).filter((value, index, self) => self.indexOf(value) === index).sort();
 };
 
 const renderChart = (chartNode, data) => {
-  const chart = window.echarts.init(chartNode);
+  chart = chart ? chart : window.echarts.init(chartNode);
   const option = {
     legend: { show: true },
     xAxis: {
@@ -62,6 +63,10 @@ const renderChart = (chartNode, data) => {
     },
     yAxis: {
       type: 'value',
+      name: "USD$ millions (constant 2019 prices)",
+      nameLocation: "middle",
+      nameGap: 50
+
     },
     series: [
       {
@@ -97,6 +102,7 @@ const init = (className) => {
            * const chart = window.echarts.init(chartNode);
            */
 
+          const defaultCountry = 'United States';
           dichart.showLoading();
           if (window.DIState) {
             window.DIState.addListener(() => {
@@ -104,9 +110,15 @@ const init = (className) => {
               const state = window.DIState.getState;
               const { country, dataOne: data } = state;
               if (country && data) {
-                console.log(country, data);
+                const countryData = filterDataByPurpose(
+                  filterDataByCountry(data, country || defaultCountry, COUNTRY_FIELD),
+                  PURPOSE_TO_FILTER_BY,
+                  PURPOSE_FIELD,
+                );
+                console.log(countryData);
                 // chart goes here
-                renderChart(chartNode, data);
+                renderChart(chartNode, countryData);
+                // extractChartFamilyPlanningData(data);
 
                 dichart.hideLoading();
               }
