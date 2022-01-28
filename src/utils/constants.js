d63e24803dea5273fb3d94b4089a5a24e8882342
dean-breed <dean.breed@devinit.org>
2022-01-28 12:43:08 +0000

    Changing for IATI data and putting data in public repo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const COUNTRY_FIELD = 'donor_name';
+export const COUNTRY_FIELD = 'Reporting Organisation Narrative';
 export const PURPOSE_FIELD = 'purpose_name';
 export const PURPOSE_TO_FILTER_BY = ['Reproductive health care', 'Family planning'];
-export const VALUE_FIELD = 'usd_disbursement_deflated';
+export const VALUE_FIELD = 'x_transaction_value_usd_Sum';
 export const YEARS = [2019, 2021];
