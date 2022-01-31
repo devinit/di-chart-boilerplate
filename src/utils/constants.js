a9684fa531d46de43f67d95be22975d206825e5b
David Ebukali <debukali@gmail.com>
2022-01-31 10:57:26 +0300

    Add html and js to render table three

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,7 @@ export const DEFAULT_COUNTRY = 'United States';
 export const COUNTRY_FIELD = 'donor_name';
 export const PURPOSE_FIELD = 'purpose_name';
 export const PURPOSE_TO_FILTER_BY = ['Reproductive health care', 'Family planning'];
+export const ALTERNATIVE_PURPOSE_TO_FILTER_BY = ['Reproductive health care and family planning', 'Reproductive health care', 'Family planning'];
 export const VALUE_FIELD = 'usd_disbursement_deflated';
+export const CHANNEL_FIELD = 'DI channel (Abreviated)';
 export const YEARS = [2010, 2019];
