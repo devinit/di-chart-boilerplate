5e110285bceedc52d0786b0b49010cae394e8481
David Ebukali <debukali@gmail.com>
2022-02-03 11:24:46 +0300

    Render oda by aid type table

--- src/components/OdaAidTable.js
+++ src/components/OdaAidTable.js
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types';
 import Table from './Table';
 
 const OdaAidTable = (props) => {
-  console.log(props);
+  const renderRows = (rows, header = false) =>
+    rows.map((row, index) => (
+      <tr key={index}>{row.map((cell, key) => (header ? <th key={key}>{cell}</th> : <td key={key}>{cell}</td>))}</tr>
+    ));
 
   return (
     <Table>
       <thead>
-        <tr><td>ODA Table Goes Here</td></tr>
+      {renderRows(
+          props.rows.filter((row, index) => index === 0),
+          true,
+        )}
       </thead>
+      <tbody>{renderRows(props.rows.filter((row, index) => index > 0))}</tbody>
     </Table>
   );
 };
