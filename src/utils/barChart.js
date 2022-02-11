cf1bd72885685c682d9367fb6602c62b86dec032
David Ebukali <debukali@gmail.com>
2022-02-11 08:39:45 +0300

    Refactor barChartOne util file

--- src/utils/barChart.js
+++ src/utils/barChart.js
@@ -7,30 +7,3 @@ export const extractChartData = (data, purpose, years, valueField, purposeField)
 
   return getYearRangeDataAsSum(filteredData, years, valueField);
 };
-
-export const groupAidTypeSums = (chartData) => {
-  let chartTotals = {};
-  for (let i = 0; i < chartData.length; i++) {
-    const chartDataItem = chartData[i];
-    for (let k = 0; k < chartData.length; k++) {
-      chartTotals[k] = chartTotals[k] ? chartTotals[k] : [];
-      chartTotals[k].push(chartDataItem[k]);
-    }
-  }
-
-  return chartTotals;
-};
-
-export const getPercentages = (chartData, groupedSums) => {
-  const percentages = Object.keys(groupedSums).map((item) => {
-    return { 
-      [item]: groupedSums[item].reduce((acc, item) => acc + item, 0)
-    };
-  });
-
-  return chartData.map((item) => {
-    return item.map((arr, index) => {
-       return (((percentages[index] ? parseFloat(arr) : 0)/parseFloat(percentages[index] ? percentages[index][index] : 1)))*100; 
-    }).filter((percentage) => percentage !== 0);
-  });
-}
