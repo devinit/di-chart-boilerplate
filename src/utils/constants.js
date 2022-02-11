0cc3e10cfd37dbdb9d0ef71ded09c319f8518672
David Ebukali <debukali@gmail.com>
2022-02-11 08:44:05 +0300

    Remove duplicated constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,6 +7,5 @@ export const ALTERNATIVE_PURPOSE_TO_FILTER_BY = ['Reproductive health care and f
 export const VALUE_FIELD = 'usd_disbursement_deflated';
 export const CHANNEL_FIELD = 'oecd_aggregated_channel';
 export const CHANNEL_VALUE_FIELD = 'usd_disbursement_deflated_Sum';
-export const AIDTYPE_VALUE_FIELD = 'usd_disbursement_deflated_Sum';
 export const AIDTYPE_PURPOSE_FIELD = 'aid_type_di_name';
 export const YEARS = [2010, 2019];
