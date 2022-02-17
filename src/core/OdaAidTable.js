a4fd449263231fb9a454e6df7cc46f8a4f9eaacb
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-02-17 17:13:29 +0300

    Format Aid Type table values

--- src/core/OdaAidTable.js
+++ src/core/OdaAidTable.js
@@ -1,12 +1,15 @@
 import { createElement } from 'react';
 import { render } from 'react-dom';
 import { OdaAidTable } from '../components/OdaAidTable';
-import { COUNTRY_FIELD, DEFAULT_COUNTRY } from '../utils/constants';
-import { filterDataByCountry } from '../utils/data';
+import { COUNTRY_FIELD, DEFAULT_COUNTRY, PURPOSE_FIELD } from '../utils/constants';
+import { filterDataByCountry, filterDataByPurpose, formatNumber } from '../utils/data';
 import { addFilter, addFilterWrapper } from '../widgets/filters';
 // import d3 from 'd3'; // eslint-disable-line import/no-unresolved
 
 const YEAR = 2019;
+const AIDTYPE_FIELD = 'aid_type_di_name';
+const VALUE_FIELD = 'usd_disbursement_deflated_Sum';
+
 const getPurposeNames = (data) => {
   const purposeNames = [];
   data.forEach((record) => {
@@ -17,35 +20,32 @@ const getPurposeNames = (data) => {
 
   return purposeNames;
 };
-const filterDataByPurpose = (data, purpose) => data.filter((item) => item.purpose_name === purpose);
 const filterDataByYear = (data) => data.filter((item) => item.year === YEAR);
 
 const getRows = (unfilteredData, data) => {
-  const headerRow = [['Aid Type', '2019', '% Total']];
+  const headerRow = [['Aid Type', YEAR, '% Total']];
   const allRowLabels = unfilteredData.reduce((acc, item) => {
-    if(!acc.includes(item.aid_type_di_name)) {
-      acc.push(item.aid_type_di_name);
+    if(!acc.includes(item[AIDTYPE_FIELD])) {
+      acc.push(item[AIDTYPE_FIELD]);
     }
 
     return acc;
   }, []);
-  const totalDisbursments = data
-    .map((item) => item.usd_disbursement_deflated_Sum)
-    .reduce((prev, current) => prev + current, 0);
+  const totalDisbursments = data.map((item) => item[VALUE_FIELD]).reduce((prev, current) => prev + current, 0);
   const rows = allRowLabels.map((label) => {
-    const row = data.find((item) => item.aid_type_di_name === label);
-    const rowValue = row ? row.usd_disbursement_deflated_Sum : 0;
-    const rowPercentage = `${(((rowValue / totalDisbursments)*100) || 0).toFixed(1)}%`;
+    const row = data.find((item) => item[AIDTYPE_FIELD] === label);
+    const rowValue = row ? row[VALUE_FIELD] : 0;
+    const rowPercentage = `${(formatNumber((rowValue / totalDisbursments)*100) || 0)}%`;
 
-    return [label].concat(rowValue.toFixed(1), [rowPercentage]);
+    return [label].concat(formatNumber(rowValue), [rowPercentage]);
   });
 
-  return headerRow.concat(rows, [['Grand Total', totalDisbursments.toFixed(1), '100%']]);
+  return headerRow.concat(rows, [['Total', formatNumber(totalDisbursments), '100%']]);
 };
 
 const renderTable = (tableNode, data, country, purpose) => {
   const countryData = filterDataByCountry(data, country || DEFAULT_COUNTRY, COUNTRY_FIELD);
-  const purposeFilteredData = filterDataByPurpose(countryData, purpose);
+  const purposeFilteredData = filterDataByPurpose(countryData, purpose, PURPOSE_FIELD);
   const rows = getRows(data, filterDataByYear(purposeFilteredData));
   render(createElement(OdaAidTable, { country, rows }), tableNode);
 };
