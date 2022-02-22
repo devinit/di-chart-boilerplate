fd019ea742e9d0e2f85217cd23967ecfea89d8cc
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-02-22 16:55:49 +0300

    Fetch IATI data

---
+++ src/core/iati/index.js
@@ -0,0 +1,12 @@
+import { fetchCoreData } from '../../utils/data';
+
+const init = () => {
+  fetchCoreData([
+    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/IATI-RHFP-data-v1.csv', state: 'dataOne' },
+    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/IATI-RHFP-data-v2.csv', state: 'dataTwo' },
+    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/iati_rhfp3.csv', state: 'dataThree' },
+    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/iati_rhfp4.csv', state: 'dataFour' },
+  ]);
+};
+
+export default init;
