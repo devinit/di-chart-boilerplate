26e5cf6d17ec057fa4303737dbedde616f293f89
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-07-18 14:24:56 +0300

    Add data utils

---
+++ src/utils/data.js
@@ -0,0 +1,15 @@
+import { parse } from 'papaparse';
+
+export const ACTIVE_BRANCH = 'main';
+
+const fetchCSVData = (url) =>
+  new Promise((resolve) => {
+    parse(url, {
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+      complete: ({ data }) => resolve(data),
+    });
+  });
+
+export default fetchCSVData;
