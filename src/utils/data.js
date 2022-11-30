b54bdd45636582cc584476d50fe5fcb878b053dd
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-11-30 08:42:08 +0300

    Copy vital code from gates repo

--- src/utils/data.js
+++ src/utils/data.js
@@ -12,4 +12,20 @@ const fetchCSVData = (url) =>
     });
   });
 
+export const formatNumber = (value, defaultForNan = '') => {
+  const formattedNumber = Number(value);
+
+  if (Number.isNaN(formattedNumber)) {
+    return defaultForNan;
+  }
+
+  return new Intl.NumberFormat('en-IN', { maximumFractionDigits: 2 }).format(formattedNumber);
+};
+
+export const parseValuesToNumbers = (data, valueField) =>
+  data.map((item) => ({
+    ...item,
+    [valueField]: item[valueField] ? Number(item[valueField]) : 'NA',
+  }));
+
 export default fetchCSVData;
