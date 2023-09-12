47d68548df14e654b7c21eb02f34e302c4e55906
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-09-12 13:25:24 +0300

    Add option to adjust width of the preview window

--- src/components/ChartViews.js
+++ src/components/ChartViews.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 const ChartView = () => {
   const [view, setView] = useState('desktop');
+  const [isValidInput, setIsValidInput] = useState(true);
+
+  const chartPreview = document.getElementById('preview-iframe');
 
   useEffect(() => {
-    const chartPreview = document.getElementById('preview-iframe');
-
     if (view === 'mobile') {
-      chartPreview.style.width = '300px';
+      chartPreview.style.width = '500px';
     }
     if (view === 'desktop') {
       chartPreview.style.width = '1400px';
@@ -16,6 +17,23 @@ const ChartView = () => {
       chartPreview.style.width = '820px';
     }
   }, [view]);
+
+  const validateInput = (inputString) => {
+    const reg = /^(\d+)px$/;
+    const isValid = inputString.length === 0 || reg.test(inputString.trim());
+
+    return isValid;
+  };
+
+  const handleChange = (event) => {
+    const validationResult = validateInput(event.target.value);
+    setIsValidInput(validationResult);
+    setView(event.target.value);
+  };
+
+  const handleClick = () => {
+    chartPreview.style.width = view;
+  };
 
   return (
     <div>
@@ -28,6 +46,18 @@ const ChartView = () => {
       <button className="button" onClick={() => setView('mobile')}>
         Mobile
       </button>
+      <label htmlFor="preview-width" id="preview-width-label">
+        Enter screen width
+      </label>
+      <div id="width-input">
+        <div id="preview-width-input">
+          <input placeholder="E.g 300px" id="preview-width" onChange={(event) => handleChange(event)}></input>
+          <button className="button" id="preview-button" onClick={handleClick}>
+            ENTER
+          </button>
+        </div>
+        {!isValidInput && <small className="preview-input-error">Please enter a valid width in the format 100px</small>}
+      </div>
     </div>
   );
 };
