c53081ae26319da7b9cf105a4236324f58db993a
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-09-22 13:04:41 +0300

    Remove option to enter screen size

--- src/components/ChartViews.js
+++ src/components/ChartViews.js
@@ -5,7 +5,6 @@ const ChartView = () => {
   const width = new PreviewWidth();
 
   const [view, setView] = useState('desktop');
-  const [isValidInput, setIsValidInput] = useState(true);
   const [checked, setChecked] = useState(true);
   const [display, setDisplay] = useState('flex');
 
@@ -22,23 +21,6 @@ const ChartView = () => {
       chartPreview.style.width = width.getWidth('tablet');
     }
   }, [view]);
-
-  const validateInput = (inputString) => {
-    const reg = /^(\d+)px$/;
-    const isValid = inputString.length === 0 || reg.test(inputString.trim());
-
-    return isValid;
-  };
-
-  const handleInputChange = (event) => {
-    const validationResult = validateInput(event.target.value);
-    setIsValidInput(validationResult);
-    setView(event.target.value);
-  };
-
-  const onEnterButtonClicked = () => {
-    chartPreview.style.width = view;
-  };
 
   const container = document.getElementById('page-container');
   container.style.display = display;
@@ -75,18 +57,6 @@ const ChartView = () => {
           {checked ? 'Hide Preview' : 'Show Preview'}{' '}
           <input type="checkbox" checked={checked} value={checked} onChange={() => handleToggleChange()} />
         </label>
-      </div>
-      <label htmlFor="preview-width" id="preview-width-label">
-        Enter screen width
-      </label>
-      <div id="width-input">
-        <div id="preview-width-input">
-          <input placeholder="E.g 300px" id="preview-width" onChange={(event) => handleInputChange(event)}></input>
-          <button className="button" id="preview-button" onClick={onEnterButtonClicked}>
-            ENTER
-          </button>
-        </div>
-        {!isValidInput && <small className="preview-input-error">Please enter a valid width in the format 100px</small>}
       </div>
     </div>
   );
