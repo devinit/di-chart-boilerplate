e8507d268d1186e43b8c5ae59ff3394c26f6654f
ThatcherK <kaliithatmau@gmail.com>
2023-10-13 10:30:25 +0300

    Point to current branch

--- src/core/oecd/index.js
+++ src/core/oecd/index.js
@@ -12,9 +12,9 @@ import { fetchCoreData } from '../../utils/data';
 
 const init = () => {
   fetchCoreData([
-    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/data_1.csv', state: 'dataOne' },
-    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/data_2.csv', state: 'dataTwo' },
-    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/main/2022/data_3.csv', state: 'dataThree' },
+    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/feature/update-rhfp-files/2022/data_1.csv', state: 'dataOne' },
+    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/feature/update-rhfp-files/2022/data_2.csv', state: 'dataTwo' },
+    { url: 'https://raw.githubusercontent.com/devinit/di-website-data/feature/update-rhfp-files/2022/data_3.csv', state: 'dataThree' },
   ], { country: 'United States' });
   initDonorFilter('dicharts--donor-selector');
   initTableOne('dicharts--table-one');
